b120206370da64fb6acb5b3b0129fc60cf131b1c
KeremSahin22 <keremsahin1905@gmail.com>
2026-07-03 17:11:35 -0400

    Update wandb_sync.sh to correct WANDB_DIR path for synchronization
    - Adjusted the WANDB_DIR variable in the `wandb_sync.sh` script to include an additional 'wandb' directory, ensuring accurate synchronization of wandb offline runs.

--- scripts/wandb_sync.sh
+++ scripts/wandb_sync.sh
@@ -6,7 +6,7 @@
 module load miniforge3/23.11.0-0
 conda activate /ccs/home/kerem.sahin/.conda/envs/olmo_pretraining
 
-WANDB_DIR=/lustre/orion/lrn089/scratch/kerem.sahin/checkpoints/olmo1b-frontier/wandb
+WANDB_DIR=/lustre/orion/lrn089/scratch/kerem.sahin/checkpoints/olmo1b-frontier/wandb/wandb
 INTERVAL=${1:-60}
 
 echo "Syncing $WANDB_DIR every ${INTERVAL}s. Ctrl-C to stop."
